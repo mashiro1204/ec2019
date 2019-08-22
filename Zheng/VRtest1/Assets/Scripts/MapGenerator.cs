3b331994d75af568b49856fc4c5512e6f6098a0a
mashiro1204 <c4pump@gmail.com>
2019-08-22 15:36:38 +0900

    just test

---
+++ Zheng/VRtest1/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    public Transform prefabTile;
+    public Vector2 mapSize;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
